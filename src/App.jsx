8e5c1f125ad91b19ca74ca02cafcd3dd2e9d32c3
kekelberi <alexei.gaponov@gmail.com>
2022-10-10 22:44:27 -0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { createSignal } from "solid-js";
+import Timer from "./components/Timer";
 
 function App() {
-  const [count, setCount] = createSignal(0)
-  
-  
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>hello my friend</p>
-        {count}
+        <div>
+          <img src={logo} class={styles.logo} alt="logo" />
+          <p>hello my friend</p>
+          <div>
+            <Timer />
+          </div>
+        </div>
       </header>
     </div>
   );
