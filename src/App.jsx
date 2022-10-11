8909bb20cf07f50b2e66f031168fed909df0a460
kekelberi <alexei.gaponov@gmail.com>
2022-10-10 22:34:24 -0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
+import { createSignal } from "solid-js";
 
 function App() {
+  const [count, setCount] = createSignal(0)
+  
+  
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>hello my friend</p>
+        {count}
       </header>
     </div>
   );
